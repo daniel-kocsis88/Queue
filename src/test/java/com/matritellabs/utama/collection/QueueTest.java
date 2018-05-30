638cfa01f38627c4af1cfe7e5e592ee51a5ad006
dtorok_local <dtorok@gmail.com>
2018-05-30 09:33:11 +0200

    first commit on my side

--- src/test/java/com/matritellabs/utama/collection/QueueTest.java
+++ src/test/java/com/matritellabs/utama/collection/QueueTest.java
@@ -1,4 +1,25 @@
 package com.matritellabs.utama.collection;
 
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class QueueTest {
+
+    @Test
+    public void testAddString() {
+        QueueClass qc = new QueueClass(10);
+        assertEquals(true, qc.add("sheep"));
+        assertEquals(true, qc.add("giraffe"));
+        assertEquals(true, qc.add("sloth"));
+        assertEquals(true, qc.add("tamandua"));
+        assertEquals(true, qc.add("raccoon"));
+        assertEquals(true, qc.add("hamster"));
+        assertEquals(true, qc.add("horse"));
+        assertEquals(true, qc.add("rat"));
+        assertEquals(true, qc.add("m"));
+        assertEquals(true, qc.add("rat"));
+    }
+
+
 }
