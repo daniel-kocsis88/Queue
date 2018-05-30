a5e606be73ab7473b02fc6c7f790e66150ad5df0
daniel-kocsis88 <kocsis.daniel4@gmail.com>
2018-05-30 09:21:28 +0200

    fields added

--- src/main/java/com/matritellabs/utama/collection/QueueClass.java
+++ src/main/java/com/matritellabs/utama/collection/QueueClass.java
@@ -1,6 +1,14 @@
 package com.matritellabs.utama.collection;
 
 public class QueueClass implements Queueu{
+    private int capacity;
+    private Object[] array;
+
+    public QueueClass(int capacity) {
+        this.capacity = capacity;
+        this.array = new Object[capacity];
+    }
+
     @Override
     public boolean add(Object e) {
         return false;
