7e1105b729a37ffb206626a8bfdf1e396c28450f
daniel-kocsis88 <kocsis.daniel4@gmail.com>
2018-05-30 09:00:25 +0200

    logback xml

---
+++ src/main/java/com/matritellabs/utama/collection/QueueClass.java
@@ -0,0 +1,33 @@
+package com.matritellabs.utama.collection;
+
+public class QueueClass implements Queueu{
+    @Override
+    public boolean add(Object e) {
+        return false;
+    }
+
+    @Override
+    public Object element() {
+        return null;
+    }
+
+    @Override
+    public boolean offer(Object e) {
+        return false;
+    }
+
+    @Override
+    public Object peek() {
+        return null;
+    }
+
+    @Override
+    public Object poll() {
+        return null;
+    }
+
+    @Override
+    public Object remove() {
+        return null;
+    }
+}
--- src/main/java/com/matritellabs/utama/collection/Queueu.java
+++ src/main/java/com/matritellabs/utama/collection/Queueu.java
@@ -1,5 +1,12 @@
 package com.matritellabs.utama.collection;
 
 public interface Queueu {
-    
+
+     boolean add(Object e);
+     Object element();
+     boolean offer(Object e);
+     Object peek();
+     Object poll();
+     Object remove();
+
 }
